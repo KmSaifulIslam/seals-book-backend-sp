0cc062a7c2bdfd7b9562fccd0d16e1b81a2773ed
saiful <kmsaifulislam123@gmail.com>
2025-01-26 10:19:12 +0600

    Started Project

---
+++ src/main/java/org/example/sealsbookbackendsp/SealsBookBackendSpApplication.java
@@ -0,0 +1,13 @@
+package org.example.sealsbookbackendsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SealsBookBackendSpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SealsBookBackendSpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sealsbookbackendsp/SealsBookBackendSpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sealsbookbackendsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SealsBookBackendSpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
