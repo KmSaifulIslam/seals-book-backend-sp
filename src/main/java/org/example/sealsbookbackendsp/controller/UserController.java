ceab6de235540fd0e6d6509ea200f32f5e2ec8ea
KmSaifulIslam <kmsaifulislam123@gmail.com>
2025-02-25 05:09:05 +0600

    Get user by id api created.

--- src/main/java/org/example/sealsbookbackendsp/controller/UserController.java
+++ src/main/java/org/example/sealsbookbackendsp/controller/UserController.java
@@ -1,17 +1,37 @@
 package org.example.sealsbookbackendsp.controller;
 
+import org.example.sealsbookbackendsp.model.User;
+import org.example.sealsbookbackendsp.repository.UserRepository;
+import org.example.sealsbookbackendsp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/profile")
     public ResponseEntity<String> getUserProfile() {
         return ResponseEntity.ok("User Profile Data");
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable long id) {
+        Optional<User> user = userRepository.findById(id);
+
+        return user.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
 }
