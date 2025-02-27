394c7abf8c05cd8f50acd36dd42e21c840373d24
KmSaifulIslam <kmsaifulislam123@gmail.com>
2025-02-27 20:31:53 +0600

    Code Update..

---
+++ src/main/java/org/example/sealsbookbackendsp/model/Store.java
@@ -0,0 +1,40 @@
+package org.example.sealsbookbackendsp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "store")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long storeId;
+    private String storeName;
+    private String storeDescription;
+    private String storeEmail;
+    private String storePhoneNumber;
+    private String storeAddress;
+    private String city;
+    private String state;
+    private String country;
+    private String storeLogo;
+    private String bannerUrl;
+    private String openingHours;
+    private String storeStatus;
+    private String storeType;
+    private String storeCategory;
+    private String facebookPageLink;
+    private String websiteUrl;
+    private String whatsappNumber;
+
+    private String storeOwnerType;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id", nullable = false)
+    private User storeOwner;
+}
