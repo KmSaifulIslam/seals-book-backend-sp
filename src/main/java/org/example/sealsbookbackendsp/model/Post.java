b4e5a575d6936e0ca5bc814fe4dbb80fdd18accd
KmSaifulIslam <kmsaifulislam123@gmail.com>
2025-03-20 05:26:43 +0600

    code updated...

--- src/main/java/org/example/sealsbookbackendsp/model/Post.java
+++ src/main/java/org/example/sealsbookbackendsp/model/Post.java
@@ -12,7 +12,6 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
-
     private Long storeId;
     private String title;
     private String bannerUrl;
